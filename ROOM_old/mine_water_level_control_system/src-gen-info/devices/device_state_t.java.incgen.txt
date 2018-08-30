2622d9a9c87ffc51dd79f54929bf2b1c57e7e97c
Jovan Djukic <djukicjovan56@gmail.com>
2018-08-30 22:37:40 +0200

    Deleted old model.

--- ROOM_old/mine_water_level_control_system/src-gen-info/devices/device_state_t.java.incgen.txt
+++
@@ -1,6 +0,0 @@
-package devices;
-
-public interface device_state_t {
-	static final int ON = 0;
-	static final int OFF = 1;
-}
--- ROOM_old/org.eclipse.etrice.runtime.java/src/org/eclipse/etrice/runtime/java/modelbase/IInterfaceItem.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.etrice.runtime.java.modelbase;
-
-import org.eclipse.etrice.runtime.java.messaging.IRTObject;
-
-public interface IInterfaceItem extends IRTObject {
-
-	IInterfaceItem connectWith(IInterfaceItem peer);
-}
